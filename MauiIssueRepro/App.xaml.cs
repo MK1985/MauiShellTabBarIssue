b633fe8ac20c2f263c0c8c8f8b69bf69afb8ce7d
maciej.koper@partners.mbank.pl <maciej.koper@partners.mbank.pl>
2022-12-14 13:39:33 +0100

    first commit

---
+++ MauiIssueRepro/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiIssueRepro;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiIssueRepro/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiIssueRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
